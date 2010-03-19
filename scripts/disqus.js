b5d90857f68a26ce24272a37277ea3b55b734604
Tomohiro, TAIRA <tomohiro.t+github@gmail.com>
2010-03-19 10:13:40 +0900

    rename Disqus shortname

--- scripts/disqus.js
+++ scripts/disqus.js
@@ -2,9 +2,9 @@
     var links = document.getElementsByTagName('a');
     var query = '?';
     for(var i = 0; i < links.length; i++) {
-    if(links[i].href.indexOf('#disqus_thread') >= 0) {
-        query += 'url' + i + '=' + encodeURIComponent(links[i].href) + '&';
+        if(links[i].href.indexOf('#disqus_thread') >= 0) {
+            query += 'url' + i + '=' + encodeURIComponent(links[i].href) + '&';
+        }
     }
-    }
-    document.write('<script charset="utf-8" type="text/javascript" src="http://disqus.com/forums/tomohiro/get_num_replies.js' + query + '"></' + 'script>');
+    document.write('<script charset="utf-8" type="text/javascript" src="http://disqus.com/forums/tomohiro-github-com/get_num_replies.js' + query + '"></' + 'script>');
 })();
